fd70422e81b681a338b9075c69027a22fe41660f
yoshutch <yoshutch@gmail.com>
2020-06-01 15:58:16 -0600

    switching to yarn

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,11 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
 
-async function run(): Promise<void> {
+function run(): void {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`)
+    const masterPassword: string = core.getInput('maven-master-password')
 
+    core.debug(`Got input master password ${masterPassword}`) // TODO remove
     core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    core.setOutput('time', new Date().toTimeString())
   } catch (error) {
     core.setFailed(error.message)
   }
