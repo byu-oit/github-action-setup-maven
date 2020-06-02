a87765200060ace55fa81304fa637882cfba66df
yoshutch <yoshutch@gmail.com>
2020-06-02 08:44:45 -0600

    updated gitignore, trying to debug maven_home env var

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,11 @@ import * as core from '@actions/core'
 
 function run(): void {
   try {
-    const masterPassword: string = core.getInput('maven-master-password')
+    const masterPassword: string = core.getInput('maven-master-password', {
+      required: true
+    })
+    const mavenHome = process.env['MAVEN_HOME'] || ''
+    core.info(`mavenHome ${mavenHome}`)
 
     core.debug(`Got input master password ${masterPassword}`) // TODO remove
     core.debug(new Date().toTimeString())
