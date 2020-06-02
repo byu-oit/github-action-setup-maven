cf60e11ffe70dc6d57a7d92dc7d67539f5b39a3d
yoshutch <yoshutch@gmail.com>
2020-06-02 09:39:36 -0600

    download settings file and write to .m2/settings.xml

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,21 @@
 import * as core from '@actions/core'
+import * as httpm from '@actions/http-client'
+import * as fs from 'fs'
 
-function run(): void {
+async function run(): Promise<void> {
   try {
     // const masterPassword: string = core.getInput('maven-master-password', {
     //   required: true
     // })
-    const mavenHome = process.env['MAVEN_HOME'] || ''
-    core.debug(`mavenHome ${mavenHome}`)
 
+    const http = new httpm.HttpClient('setup-maven', undefined, {
+      allowRetries: true,
+      maxRetries: 3
+    })
+    const mavenSettingsUrl =
+      'https://byu-oit.github.io/byu-apps-custom-cicd-resources/maven-settings.xml'
+    const resp = await http.get(mavenSettingsUrl)
+    fs.writeFileSync('$HOME/.m2/settings.xml', await resp.readBody())
     core.debug(new Date().toTimeString())
   } catch (error) {
     core.setFailed(error.message)
