fe3f3aed2ab63286c72bb45884303e437114ff21
yoshutch <yoshutch@gmail.com>
2020-06-02 08:52:11 -0600

    debug maven home

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,12 @@ import * as core from '@actions/core'
 
 function run(): void {
   try {
-    const masterPassword: string = core.getInput('maven-master-password', {
-      required: true
-    })
+    // const masterPassword: string = core.getInput('maven-master-password', {
+    //   required: true
+    // })
     const mavenHome = process.env['MAVEN_HOME'] || ''
-    core.info(`mavenHome ${mavenHome}`)
+    core.debug(`mavenHome ${mavenHome}`)
 
-    core.debug(`Got input master password ${masterPassword}`) // TODO remove
     core.debug(new Date().toTimeString())
   } catch (error) {
     core.setFailed(error.message)
